d39d2e4f06d87469ae3c55144b53eff2695f96c0
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2024-12-24 17:36:38 +0700

    menambah dan edit alamat navigasi dan isi nya

---
+++ app/src/main/java/com/example/ucp2_roomdatabase_051/ui/navigation/AlamatNavigasi.kt
@@ -0,0 +1,37 @@
+package com.example.ucp2_roomdatabase_051.ui.navigation
+
+interface AlamatNavigasi {
+    val route: String
+}
+
+object DestinasiInsertDr: AlamatNavigasi {
+    override val route: String = "insert_dokter"
+}
+
+object DestinasiInsertJd: AlamatNavigasi {
+    override val route: String = "insert_Jd"
+}
+
+object DestinasiHome: AlamatNavigasi {
+    override val route: String = "home"
+}
+
+object DestinasiDokter: AlamatNavigasi {
+    override val route: String = "dokter"
+}
+
+object DestinasiJadwal: AlamatNavigasi {
+    override val route: String = "jadwal"
+}
+
+object DestinasiUpdate: AlamatNavigasi {
+    override val route: String = "update_jadwal"
+    const val idAntrian = "idAntrian"
+    val routeWithArg = "$route/{$idAntrian}"
+}
+
+object DestinasiDetail: AlamatNavigasi {
+    override val route: String = "detail_Jadwal"
+    const val idAntrian = "idAntrian"
+    val routeWithArg = "$route/{$idAntrian}"
+}
